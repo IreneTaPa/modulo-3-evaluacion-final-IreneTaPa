3f007d1a8514ea6f9a94fcb401baad0689957b6d
IreneTaPa <italavero@hotmail.com>
2023-07-04 09:46:22 +0200

    added copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import '../styles/layout/Footer.scss';
 
 const Footer = () => {
   return (
-    <img className="footer footer__img" src={footerImg} alt="Rick and Morty" />
+    <div>
+      <img
+        className="footer footer__img"
+        src={footerImg}
+        alt="Rick and Morty"
+      />
+      <small className="small">Made with 💚 by Irene </small>
+    </div>
   );
 };
 
